b8941e0cdbd96c71fdd316d6d11d6611837331df
Sbr <shabbir.endeavor@gmail.com>
2020-09-23 12:27:30 +0530

    Updates

---
+++ precache-manifest.fb08a9d808e1f09f1a9dcb6fb5ee0852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6336082b521ae3d5e17269d100c505",
+    "url": "/protons_react_cards/index.html"
+  },
+  {
+    "revision": "c06c068304f3f35df06d",
+    "url": "/protons_react_cards/static/css/main.71ad67df.chunk.css"
+  },
+  {
+    "revision": "bc0a8f580e84e7bd3d4f",
+    "url": "/protons_react_cards/static/js/2.cfac9d7a.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/protons_react_cards/static/js/2.cfac9d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06c068304f3f35df06d",
+    "url": "/protons_react_cards/static/js/main.36f30ef0.chunk.js"
+  },
+  {
+    "revision": "a05fdbeed61121bf571b",
+    "url": "/protons_react_cards/static/js/runtime-main.6c2e604d.js"
+  }
+]);
